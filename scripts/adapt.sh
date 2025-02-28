1d70bfeefb39479fb01384187a95a695bc7ffb20
Sammy de Figueiredo <sammy.defigueiredo@belgiantrain.be>
2025-02-28 11:55:43 +0100

    ttt

--- scripts/adapt.sh
+++ scripts/adapt.sh
@@ -1,9 +1,13 @@
 LOCATION=$1
 
+cat ${LOCATION}
+
 FILE_PATH=Recursi/vnet.yaml
 
 
 
 cp ./templates/vnet.yaml $FILE_PATH
 
-yq --inplace ".spec.location = \"${LOCATION}\"" $FILE_PATH
+yq --inplace ".spec.forProvider.location = \"${LOCATION}\"" $FILE_PATH
+
+cat $FILE_PATH
