0243ade18cf2b31c5460445d25c220ef00318746
Sammy de Figueiredo <sammy.defigueiredo@belgiantrain.be>
2025-03-06 15:54:54 +0100

    dh

--- scripts/update-repo.sh
+++ scripts/update-repo.sh
@@ -6,6 +6,5 @@ echo ${NAME}
 FILE_PATH=Recursi/repos/${NAME}.yaml
 
 yq --inplace ".spec.forProvider.name = \"${RENAME}\"" $FILE_PATH
-yq --inplace ".metadata.name = \"${RENAME}\"" $FILE_PATH
 
 cat $FILE_PATH
